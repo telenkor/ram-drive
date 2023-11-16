f5af1b0877a9a3fc1691525ae8f8c0a2adbbec32
divrn <31967374+divrn@users.noreply.github.com>
2023-11-16 13:29:52 +0300

    Add files via upload

---
+++ Script Libraries/Myriad Tables Lib.scptd/Contents/Frameworks/SMSTableDialogBuilder.framework/Versions/A/Headers/SMSTableDialogBuilder.h
@@ -0,0 +1,20 @@
+//
+//  SMSTableDialogBuilder.h
+//  SMSTableDialogBuilder
+//
+//  Created by Shane Stanley on 11/01/2016.
+//  Copyright © 2016 Shane Stanley. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <SMSTableDialogBuilder/SMSTableDialogController.h>
+
+//! Project version number for SMSTableDialogBuilder.
+FOUNDATION_EXPORT double SMSTableDialogBuilderVersionNumber;
+
+//! Project version string for SMSTableDialogBuilder.
+FOUNDATION_EXPORT const unsigned char SMSTableDialogBuilderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SMSTableDialogBuilder/PublicHeader.h>
+
+
